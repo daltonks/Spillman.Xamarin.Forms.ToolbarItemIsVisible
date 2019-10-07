c68f3f9f3d5f875d412857b0ed99d9a607f21056
Dalton Spillman <dalton_spillman@yahoo.com>
2019-10-06 21:49:09 -0500

    Initial code commit (WIP)

---
+++ Spillman.Xamarin.Forms.ToolbarItemIsVisible/ElementExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace Spillman.Xamarin.Forms
+{
+    internal static class ElementExtensions
+    {
+        public static T GetAncestor<T>(this Element element) where T : Element
+        {
+            var type = typeof(T);
+
+            var ancestor = element.Parent;
+            while (true)
+            {
+                if (ancestor == null)
+                {
+                    return null;
+                }
+
+                if (type.IsInstanceOfType(ancestor))
+                {
+                    return (T) ancestor;
+                }
+
+                ancestor = ancestor.Parent;
+            }
+        }
+    }
+}
---
+++ Spillman.Xamarin.Forms.ToolbarItemIsVisible/ToolbarItemIsVisible.cs
@@ -0,0 +1,56 @@
+﻿using Xamarin.Forms;
+
+namespace Spillman.Xamarin.Forms
+{
+    public class ToolbarItemIsVisible
+    {
+        public static readonly BindableProperty IsVisibleProperty = BindableProperty.CreateAttached(
+            "IsVisible", 
+            typeof(bool), 
+            typeof(ToolbarItemIsVisible), 
+            true,
+            propertyChanged: OnIsVisibleChanged
+        );
+
+        public static bool GetIsVisible(BindableObject view)
+        {
+            return (bool)view.GetValue(IsVisibleProperty);
+        }
+
+        public static void SetIsVisible(BindableObject view, bool value)
+        {
+            view.SetValue(IsVisibleProperty, value);
+        }
+
+        private static void OnIsVisibleChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var toolbarItem = bindable as ToolbarItem;
+
+            if (toolbarItem == null)
+            {
+                return;
+            }
+
+            var isVisible = (bool) newValue;
+
+            Device.BeginInvokeOnMainThread(() => {
+                var page = toolbarItem.GetAncestor<Page>();
+                if (page == null)
+                {
+                    return;
+                }
+
+                var toolbarItems = page.ToolbarItems;
+
+                if (isVisible && !toolbarItems.Contains(toolbarItem))
+                {
+                    toolbarItems.Add(toolbarItem);
+                }
+                else if (!isVisible && toolbarItems.Contains(toolbarItem))
+                {
+                    toolbarItems.Remove(toolbarItem);
+                }
+            });
+        }
+    }
+}
